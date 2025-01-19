76d9eaca054d515fd5c5e190e13d09b5685c60b2
roy.crippen4 <roy.crippen4@archarithms.com>
2025-01-18 23:20:40 -0500

    chore: rm comment

--- lua/inspector/init.lua
+++ lua/inspector/init.lua
@@ -9,11 +9,3 @@ M.inspect_in_float = float.inspect_in_float
 function M.setup() end
 
 return M
-
--- require('which-key').add({
---   {
---     mode = 'n',
---     { '<leader>iw', inspect_in_split, desc = '[I]nspect word', icon = '' },
---     { '<leader>if', inspect_in_float, desc = '[I]nspect in float', icon = '' },
---   },
--- })
