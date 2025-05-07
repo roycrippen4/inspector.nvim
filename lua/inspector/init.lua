7e3be7c036eb26b4e24be4a5876282af0290f160
roy.crippen4 <roy.crippen4@archarithms.com>
2025-05-07 12:33:17 -0400

    feat: InspectFloat and InspectSplit user commands

--- lua/inspector/init.lua
+++ lua/inspector/init.lua
@@ -6,6 +6,9 @@ local M = {}
 M.inspect_in_split = split.inspect_in_split
 M.inspect_in_float = float.inspect_in_float
 
+vim.api.nvim_create_user_command('InspectFloat', float.inspect_in_float, { desc = ':Inspect, but in a float that follows the mouse' })
+vim.api.nvim_create_user_command('InspectSplit', split.inspect_in_split, { desc = 'Improved `:Inspect` command' })
+
 function M.setup() end
 
 return M
